00599b1c9a98bc2ec51710a13e63ee9e6d2716d3
Tiuleev Mergen <tiuleev@sfedu.ru>
2024-08-28 23:28:34 +0300

    reinit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telethon import TelegramClient, events, sync
 import asyncio
 import time
 import os
-# Ваши данные
+import aiocron
 from dotenv import load_dotenv
 load_dotenv()
 api_id = os.getenv("API_ID")
@@ -11,19 +11,21 @@ bot_username = '@anicardplaybot'
 
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
+print('start')
 
+@aiocron.crontab('* */4 * * *')
 async def send_messages():
-    while True:  # Бесконечный цикл
-        try:
-            await client.send_message(bot_username, '🏵️ Получить карту')
-            await asyncio.sleep(1200)
-            await client.send_message(bot_username, '⚔️ Получить карту')
-        except Exception as e:
-            print(f"Ошибка: {e}")
-            os.Exit(1)
+    try:
+        await client.send_message(bot_username, '🏵️ Получить карту')
+        await asyncio.sleep(1)
+        await client.send_message(bot_username, '⚔️ Получить карту')
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        os.Exit(1)
 
 async def main():
     await send_messages()
 
+asyncio.get_event_loop().run_forever()
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
